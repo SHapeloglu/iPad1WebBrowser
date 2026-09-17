8cb57eace2b685cc693c40b72d77ff4c615d7efa
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-09-17 12:45:36 +0300

    alpha7: add redirect-aware history controller header

---
+++ HistoryBrowserViewController.h
@@ -0,0 +1,8 @@
+#import "LegacyBrowserViewController.h"
+
+@interface HistoryBrowserViewController : LegacyBrowserViewController {
+    NSString *_pendingHistoryURL;
+    NSString *_pendingHistoryTitle;
+}
+
+@end
